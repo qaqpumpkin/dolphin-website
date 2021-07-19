bfa0a418d9283508f5b4b7808f2d695c26f482f8
pumpkin <648102110@qq.com>
2021-07-19 12:01:32 +0800

    deploy

---
+++ precache-manifest.26343cf2e6b922646e40502d7e646b1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c1ec2aef8c8780a1023",
+    "url": "/dolphin-website/css/app.a81f9b5e.css"
+  },
+  {
+    "revision": "adf953d710dfc8506dcf1c793b877be0",
+    "url": "/dolphin-website/index.html"
+  },
+  {
+    "revision": "2c1ec2aef8c8780a1023",
+    "url": "/dolphin-website/js/app.41c2ac95.js"
+  },
+  {
+    "revision": "715cf5df65ba20be6856",
+    "url": "/dolphin-website/js/chunk-vendors.ad68872c.js"
+  },
+  {
+    "revision": "67911bca157902ca5a96944cf94bea58",
+    "url": "/dolphin-website/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/dolphin-website/robots.txt"
+  }
+]);
